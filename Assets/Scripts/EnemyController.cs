efafad2136244eb4ba6f50f001dcc3a1a0585ac8
Unknown <drevilbrain@gmail.com>
2018-12-01 00:23:51 -0500

    Basic player and AI movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public float lookRadius;
+    public float turnSpeed;
+    private Transform target;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if(distance <= agent.stoppingDistance)
+            {
+                // attack player
+
+                // face player
+                FaceTarget();
+            }
+        }
+	}
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector2(direction.x, 0));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        // movement
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+        transform.Translate(new Vector3(horizontal, 0, vertical));
+
+        // attack
+        if (Input.GetButtonDown("Attack"))
+        {
+
+        }
+
+        // dodge
+        if (Input.GetButtonDown("Dodge"))
+        {
+            
+        }
+    }
+}
